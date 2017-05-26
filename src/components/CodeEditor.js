9a4145d3fb7586a3780ed449d8a6c2a30add729c
Severin Neumann <severin.neumann@altmuehlnet.de>
2017-05-26 16:44:00 +0200

    Fixed AutoSave

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -4,6 +4,12 @@ import PropTypes from 'prop-types'
 import Mousetrap from 'mousetrap'
 
 class CodeEditor extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.bound = false
+  }
+
   static propTypes = {
     value: PropTypes.string.isRequired,
     options: PropTypes.object.isRequired,
@@ -15,22 +21,31 @@ class CodeEditor extends React.Component {
     if (event) {
       event.preventDefault()
     }
+    // for a non obvious reason the binding does not work on componentDidMount.
+    // Until there is a solution, this works ...
+    this.bind()
     this.props.onChange(content)
   }
 
-  componentDidMount() {
-    var rcm = document.querySelector('.ReactCodeMirror')
+  bind() {
+    if (!this.bound) {
+      this.bound = true
+      var area = document.querySelector('.moustrap-auto-save-area')
 
-    Mousetrap.prototype.stopCallback = () => { return false }
+      Mousetrap.prototype.stopCallback = () => { return false }
 
-    Mousetrap(rcm).bind('enter', () => {
-      this.props.onAutoSave(event)
-      return false
-    })
+      console.log('BINDING')
+
+      Mousetrap(area).bind('enter', () => {
+        console.log('AUTOSAVE')
+        this.props.onAutoSave(event)
+        return false
+      })
+    }
   }
 
   render() {
-    return <div>{window.isTesting === true
+    return <div className="moustrap-auto-save-area">{window.isTesting === true
       ? <textarea id="contentarea" value={this.props.value} onChange={(event) => this.handleChange(event.target.value, event)} />
       : <CodeMirror value={this.props.value} onChange={(content) => this.handleChange(content)} options={this.props.options}/>
     }</div>
