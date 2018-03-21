27a5c439cd46c774bf39daae5f32f3a78b8258ca
Severin Neumann <severin.neumann@altmuehlnet.de>
2018-03-21 14:02:01 +0100

    Changed variable editor to ACE for HTML input

--- src/components/options/editor/Variable.js
+++ src/components/options/editor/Variable.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import VariableModle from '../../../models/Variable'
 import PropTypes from 'prop-types'
+import AceEditor from 'react-ace'
+
+import 'brace/theme/textmate'
+import 'brace/mode/html'
 
 class Variable extends React.Component {
   static propTypes = {
@@ -8,15 +12,30 @@ class Variable extends React.Component {
     variable: PropTypes.instanceOf(VariableModle)
   }
 
-  updateVariable(event) {
-    event.preventDefault()
-    this.props.onValueUpdate(event.target.name, event.target.value)
+  updateVariable(value) {
+    this.props.onValueUpdate(this.props.variable.name, value)
   }
 
   render() {
     return <div className="variable-box">
       <label htmlFor="variable-1">{this.props.variable.name}</label>
-      <input name={this.props.variable.name} type="text" onChange={(event) => this.updateVariable(event)} placeholder={this.props.variable.placeholder} defaultValue={this.props.variable.value}/>
+      {/* <input name={this.props.variable.name} type="text"
+        onChange={(event) => this.updateVariable(event)}
+        placeholder={this.props.variable.placeholder}
+        defaultValue={this.props.variable.value}/> */}
+      <AceEditor height="4.5em"
+        name={this.props.variable.name}
+        minLines={1}
+        maxLines="Infinity"
+        theme="textmate"
+        mode="html"
+        highlightActiveLine={false}
+        showGutter={false}
+        autoScrollEditorIntoView={true}
+        value={this.props.variable.value}
+        ref={(c) => { this.editor = c }}
+        onChange={(event) => this.updateVariable(event)}
+      />
       <div className="help">{this.props.variable.description}</div>
     </div>
   }
