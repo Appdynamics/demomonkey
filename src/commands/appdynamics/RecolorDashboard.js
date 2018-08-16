d5642cfe27e9ec29f5de732989d573b148e60d51
Severin Neumann <severin.neumann@altmuehlnet.de>
2018-08-16 13:56:59 +0200

    Update RecolorDashboard

--- src/commands/appdynamics/RecolorDashboard.js
+++ src/commands/appdynamics/RecolorDashboard.js
@@ -1,158 +1,8 @@
 import Command from '../Command'
+import Color from 'color'
 import UndoElement from '../UndoElement'
 
 class RecolorDashboard extends Command {
-  static colors = {
-    'aliceblue': '#f0f8ff',
-    'antiquewhite': '#faebd7',
-    'aqua': '#00ffff',
-    'aquamarine': '#7fffd4',
-    'azure': '#f0ffff',
-    'beige': '#f5f5dc',
-    'bisque': '#ffe4c4',
-    'black': '#000000',
-    'blanchedalmond': '#ffebcd',
-    'blue': '#0000ff',
-    'blueviolet': '#8a2be2',
-    'brown': '#a52a2a',
-    'burlywood': '#deb887',
-    'cadetblue': '#5f9ea0',
-    'chartreuse': '#7fff00',
-    'chocolate': '#d2691e',
-    'coral': '#ff7f50',
-    'cornflowerblue': '#6495ed',
-    'cornsilk': '#fff8dc',
-    'crimson': '#dc143c',
-    'cyan': '#00ffff',
-    'darkblue': '#00008b',
-    'darkcyan': '#008b8b',
-    'darkgoldenrod': '#b8860b',
-    'darkgray': '#a9a9a9',
-    'darkgreen': '#006400',
-    'darkgrey': '#a9a9a9',
-    'darkkhaki': '#bdb76b',
-    'darkmagenta': '#8b008b',
-    'darkolivegreen': '#556b2f',
-    'darkorange': '#ff8c00',
-    'darkorchid': '#9932cc',
-    'darkred': '#8b0000',
-    'darksalmon': '#e9967a',
-    'darkseagreen': '#8fbc8f',
-    'darkslateblue': '#483d8b',
-    'darkslategray': '#2f4f4f',
-    'darkslategrey': '#2f4f4f',
-    'darkturquoise': '#00ced1',
-    'darkviolet': '#9400d3',
-    'deeppink': '#ff1493',
-    'deepskyblue': '#00bfff',
-    'dimgray': '#696969',
-    'dimgrey': '#696969',
-    'dodgerblue': '#1e90ff',
-    'firebrick': '#b22222',
-    'floralwhite': '#fffaf0',
-    'forestgreen': '#228b22',
-    'fuchsia': '#ff00ff',
-    'gainsboro': '#dcdcdc',
-    'ghostwhite': '#f8f8ff',
-    'gold': '#ffd700',
-    'goldenrod': '#daa520',
-    'gray': '#808080',
-    'green': '#008000',
-    'greenyellow': '#adff2f',
-    'grey': '#808080',
-    'honeydew': '#f0fff0',
-    'hotpink': '#ff69b4',
-    'indianred': '#cd5c5c',
-    'indigo': '#4b0082',
-    'ivory': '#fffff0',
-    'khaki': '#f0e68c',
-    'lavender': '#e6e6fa',
-    'lavenderblush': '#fff0f5',
-    'lawngreen': '#7cfc00',
-    'lemonchiffon': '#fffacd',
-    'lightblue': '#add8e6',
-    'lightcoral': '#f08080',
-    'lightcyan': '#e0ffff',
-    'lightgoldenrodyellow': '#fafad2',
-    'lightgray': '#d3d3d3',
-    'lightgreen': '#90ee90',
-    'lightgrey': '#d3d3d3',
-    'lightpink': '#ffb6c1',
-    'lightsalmon': '#ffa07a',
-    'lightseagreen': '#20b2aa',
-    'lightskyblue': '#87cefa',
-    'lightslategray': '#778899',
-    'lightslategrey': '#778899',
-    'lightsteelblue': '#b0c4de',
-    'lightyellow': '#ffffe0',
-    'lime': '#00ff00',
-    'limegreen': '#32cd32',
-    'linen': '#faf0e6',
-    'magenta': '#ff00ff',
-    'maroon': '#800000',
-    'mediumaquamarine': '#66cdaa',
-    'mediumblue': '#0000cd',
-    'mediumorchid': '#ba55d3',
-    'mediumpurple': '#9370db',
-    'mediumseagreen': '#3cb371',
-    'mediumslateblue': '#7b68ee',
-    'mediumspringgreen': '#00fa9a',
-    'mediumturquoise': '#48d1cc',
-    'mediumvioletred': '#c71585',
-    'midnightblue': '#191970',
-    'mintcream': '#f5fffa',
-    'mistyrose': '#ffe4e1',
-    'moccasin': '#ffe4b5',
-    'navajowhite': '#ffdead',
-    'navy': '#000080',
-    'oldlace': '#fdf5e6',
-    'olive': '#808000',
-    'olivedrab': '#6b8e23',
-    'orange': '#ffa500',
-    'orangered': '#ff4500',
-    'orchid': '#da70d6',
-    'palegoldenrod': '#eee8aa',
-    'palegreen': '#98fb98',
-    'paleturquoise': '#afeeee',
-    'palevioletred': '#db7093',
-    'papayawhip': '#ffefd5',
-    'peachpuff': '#ffdab9',
-    'peru': '#cd853f',
-    'pink': '#ffc0cb',
-    'plum': '#dda0dd',
-    'powderblue': '#b0e0e6',
-    'purple': '#800080',
-    'rebeccapurple': '#663399',
-    'red': '#ff0000',
-    'rosybrown': '#bc8f8f',
-    'royalblue': '#4169e1',
-    'saddlebrown': '#8b4513',
-    'salmon': '#fa8072',
-    'sandybrown': '#f4a460',
-    'seagreen': '#2e8b57',
-    'seashell': '#fff5ee',
-    'sienna': '#a0522d',
-    'silver': '#c0c0c0',
-    'skyblue': '#87ceeb',
-    'slateblue': '#6a5acd',
-    'slategray': '#708090',
-    'slategrey': '#708090',
-    'snow': '#fffafa',
-    'springgreen': '#00ff7f',
-    'steelblue': '#4682b4',
-    'tan': '#d2b48c',
-    'teal': '#008080',
-    'thistle': '#d8bfd8',
-    'tomato': '#ff6347',
-    'turquoise': '#40e0d0',
-    'violet': '#ee82ee',
-    'wheat': '#f5deb3',
-    'white': '#ffffff',
-    'whitesmoke': '#f5f5f5',
-    'yellow': '#ffff00',
-    'yellowgreen': '#9acd32'
-  }
-
   constructor(dashboardId, search, replace, location) {
     super()
     this.dashboardId = dashboardId
@@ -166,11 +16,7 @@ class RecolorDashboard extends Command {
       return '#' + value
     }
 
-    if (RecolorDashboard.colors[value.toLowerCase()]) {
-      return RecolorDashboard.colors[value.toLowerCase()]
-    }
-
-    return value
+    return Color(value).rgb().string()
   }
 
   _checkDashboardId() {
@@ -184,14 +30,9 @@ class RecolorDashboard extends Command {
   _recolorTimeseriesGraph(node) {
     var svgs = node.querySelectorAll('ad-widget-timeseries-graph svg')
 
-    if (svgs.length === 0) {
-      return false
-    }
-
     var r = []
 
     svgs.forEach((svg) => {
-
       var dots = svg.querySelectorAll('path[fill="' + this.search + '"]')
 
       var lines = svg.querySelectorAll('path[stroke="' + this.search + '"]')
@@ -209,11 +50,49 @@ class RecolorDashboard extends Command {
     return r
   }
 
+  _recolorLabels(node) {
+    var labels = node.querySelectorAll('ad-widget-label')
+    var r = []
+
+    var searchedColor = Color(this.search)
+
+    labels.forEach((label) => {
+      var currentBackgroundColor = Color(label.parentElement.style.backgroundColor)
+
+      if (searchedColor.rgb().string() === currentBackgroundColor.rgb().string()) {
+        var original = label.parentElement.style.backgroundColor
+        label.parentElement.style.backgroundColor = Color(this.replace).rgb().string()
+        r.push(new UndoElement(label.parentElement.style, 'backgroundColor', original, label.parentElement.style.backgroundColor))
+      }
+    })
+    return r
+  }
+
+  _recolorImages(node) {
+    var images = node.querySelectorAll('ad-widget-image')
+
+    var r = []
+
+    images.forEach((image) => {
+      var img = image.querySelector('img:not([class])')
+      if (!img || !img.src.includes('<svg')) {
+        return
+      }
+      var original = img.src
+      img.src = img.src.replace(new RegExp('(fill|stroke)="' + this.search + '"'), '$1="' + this.replace + '"')
+      if (original !== img.src) {
+        r.push(new UndoElement(img, 'src', original, img.src))
+      }
+    })
+
+    return r
+  }
+
   apply(node, key = 'value') {
     if (!this._checkDashboardId()) {
       return false
     }
-    return this._recolorTimeseriesGraph(node)
+    return this._recolorTimeseriesGraph(node).concat(this._recolorLabels(node)).concat(this._recolorImages(node))
   }
 }
 
--- test/commands/appdynamics/RecolorDashboard.js
+++ test/commands/appdynamics/RecolorDashboard.js
@@ -2,16 +2,84 @@ import RecolorDashboard from '../../../src/commands/appdynamics/RecolorDashboard
 
 var assert = require('assert')
 
+var location = {
+  toString() {
+    return '&dashboard=1'
+  }
+}
+
+var attr1 = {'fill': 'green'}
+var attr2 = {'stroke': 'green'}
+var attr3 = {backgroundColor: 'green'}
+
+var node = {
+  querySelectorAll(query) {
+    if (query === 'ad-widget-timeseries-graph svg') {
+      return [{
+        querySelectorAll(query) {
+          if (query.includes('fill')) {
+            return [{
+              attributes: attr1,
+              setAttribute(attr, value) {
+                this.attributes[attr] = value
+              }
+            }]
+          }
+          return []
+        }
+      }, {
+        querySelectorAll(query) {
+          if (query.includes('stroke')) {
+            return [{
+              attributes: attr2,
+              setAttribute(attr, value) {
+                this.attributes[attr] = value
+              }
+            }]
+          }
+          return []
+        }
+      }]
+    }
+    if (query === 'ad-widget-label') {
+      return [{
+        parentElement: {
+          style: attr3
+        }
+      }]
+    }
+    return []
+  }
+}
 
 describe('RecolorDashboard', function () {
   describe('#apply', function () {
-    it('changes the colors of a time series graph', function () {
-      var node = {
+    it('changes the colors of a dashboard', function () {
+      attr1 = {'fill': 'green'}
+      attr2 = {'stroke': 'green'}
+      attr3 = {backgroundColor: 'green'}
 
-      }
+      var result = new RecolorDashboard('1', 'green', 'blue', location).apply(node, 'data')
 
-      new RecolorDashboard(1, 'green', 'blue').apply(node, 'data')
-      //assert.equal(href.baseVal, 'images/icon_nodetype_php_100x100.png')
+      assert.equal(result.length, 3)
+
+      assert.equal(attr1.fill, 'blue')
+      assert.equal(attr2.stroke, 'blue')
+      assert.equal(attr3.backgroundColor, 'rgb(0, 0, 255)')
+    })
+
+    it('does not change the colors of a dashboard if dashboard id does not match', function () {
+      attr1 = {'fill': 'green'}
+      attr2 = {'stroke': 'green'}
+      attr3 = {backgroundColor: 'green'}
+
+      new RecolorDashboard('2', 'green', 'blue', location).apply(node, 'data')
+
+      assert.equal(new RecolorDashboard('2', 'green', 'blue', location).apply(node, 'data'), false)
+
+      assert.equal(attr1.fill, 'green')
+      assert.equal(attr2.stroke, 'green')
+      assert.equal(attr3.backgroundColor, 'green')
     })
   })
 })
