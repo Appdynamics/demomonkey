de29d796a901dea70f5e103bd380246263fe0c01
Severin Neumann <severin.neumann@altmuehlnet.de>
2020-01-21 09:41:24 +0100

    Version Bump (6.0.0) ; minor changes

--- test/models/Ini.js
+++ test/models/Ini.js
@@ -20,24 +20,4 @@ describe('Ini', function () {
       assert.deepEqual(new Ini('a \\= b = c').parse(), { 'a ≠ b': 'c' })
     })
   })
-
-  describe('#parseNew', function () {
-    it('parses an ini file', function () {
-      assert.deepEqual(new Ini('').parseNew(), [])
-      assert.deepEqual(new Ini('a').parseNew(), { a: true })
-      assert.deepEqual(new Ini('a = false').parseNew(), { a: false })
-      assert.deepEqual(new Ini('a = null').parseNew(), { a: null })
-      assert.deepEqual(new Ini('a = b').parseNew(), { a: 'b' })
-      assert.deepEqual(new Ini('a = b; inline comment').parseNew(), { a: 'b' })
-      assert.deepEqual(new Ini('a = "; no comment"').parseNew(), { a: '; no comment' })
-      assert.deepEqual(new Ini('a[] = b\ra[] = c').parseNew(), { a: ['b', 'c'] })
-      assert.deepEqual(new Ini('a = b\r[section]\ra=b\r;comment\rx = y').parseNew(), { a: 'b', x: 'y' })
-      assert.deepEqual(new Ini('a = b\r[section]\r[section.subsection]\ra=b\r;comment\rx = y').parseNew(), { a: 'b', x: 'y' })
-    })
-
-    it('allows = to be escaped', function () {
-      assert.deepEqual(new Ini('a = b = c').parseNew(), { a: 'b = c' })
-      assert.deepEqual(new Ini('a \\= b = c').parseNew(), { 'a = b': 'c' })
-    })
-  })
 })
